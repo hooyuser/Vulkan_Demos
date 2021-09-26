83032c23f727f160c1263eaf10c96d0426d20fe8
Derived Cat <hooyuser@outlook.com>
2021-09-26 17:17:08 +0800

    add pre build command
    add pre build command. compile spirv.

--- demos/007_BRDF_2D_LUT/assets/compile_spirv.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-IF NOT EXIST "shaders" mkdir "shaders"
-cd ..\..\..\
-.\extern\vulkan\Bin32\glslc.exe %~dp0glsl_shaders\shader.vert -o %~dp0shaders\vert.spv
-.\extern\vulkan\Bin32\glslc.exe %~dp0glsl_shaders\shader.frag -o %~dp0shaders\frag.spv
-echo Compilation complete!
-pause
